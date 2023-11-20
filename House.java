de124df0122fd4ec316a5b77d8d3f171a95b5734
AyeshaSaddiqa5 <128455158+AyeshaSaddiqa5@users.noreply.github.com>
2023-11-20 15:20:58 +0600

    Add files via upload

---
+++ House.java
@@ -0,0 +1,67 @@
+abstract class House {
+    private int houseNumber;
+    private HouseStatus status;
+    private Person tenant;
+    private double rentAmount;
+    private boolean rentPaid;
+
+    public House(int houseNumber, HouseStatus status, double rentAmount) {
+        this.houseNumber = houseNumber;
+        this.status = status;
+        this.tenant = null;
+        this.rentAmount = rentAmount;
+        this.rentPaid = false;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public HouseStatus getStatus() {
+        return status;
+    }
+
+    public Person getTenant() {
+        return tenant;
+    }
+
+    public double getRentAmount() {
+        return rentAmount;
+    }
+
+    public boolean isRentPaid() {
+        return rentPaid;
+    }
+
+    public void occupyHouse(Person tenant) {
+        if (status == HouseStatus.FOR_RENT && this.tenant == null) {
+            this.tenant = tenant;
+            status = HouseStatus.OCCUPIED;
+            System.out.println("House " + houseNumber + " allocated to " + tenant.getName());
+        } else {
+            System.out.println("House " + houseNumber + " is not available for allocation.");
+        }
+    }
+
+    public void vacateHouse() {
+        if (status == HouseStatus.OCCUPIED) {
+            tenant = null;
+            status = HouseStatus.VACANT;
+            rentPaid = false;
+            System.out.println("House " + houseNumber + " vacated.");
+        } else {
+            System.out.println("House " + houseNumber + " is not occupied.");
+        }
+    }
+
+    public void payRent() {
+        if (status == HouseStatus.OCCUPIED && !rentPaid) {
+            System.out.println("Rent collected for House " + houseNumber + " from " + tenant.getName());
+            rentPaid = true;
+        } else {
+            System.out.println("House " + houseNumber + " is not available for rent collection or rent is already paid.");
+        }
+    }
+
+    public abstract void displayDetails();
+}
---
+++ HouseRentSystem.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class HouseRentSystem {
+    public static void main(String[] args) {
+        HousingSociety society = new HousingSociety();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice;
+        do {
+            System.out.println("1. Enter your name and allocate a house");
+            System.out.println("2. Collect rent");
+            System.out.println("3. Vacate a house");
+            System.out.println("4. Display houses");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice:");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    society.enterNameAndAllocateHouse();
+                    break;
+
+                case 2:
+                    society.collectRent();
+                    break;
+
+                case 3:
+                    society.vacateHouse();
+                    break;
+
+                case 4:
+                    society.displayHouses();
+                    break;
+
+                case 0:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        } while (choice != 0);
+
+        scanner.close();
+    }
+}
+
